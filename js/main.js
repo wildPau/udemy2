ebce14ba4addee8437ca6725fb69b739279fce8c
wildPau <wildukranian1@ukr.net>
2019-12-25 18:57:03 +0200

    6 lesson

---
+++ js/main.js
@@ -0,0 +1,16 @@
+let btn = document.querySelector('#start'),
+    value = document.getElementsByClassName('result-table'),
+    input = document.querySelectorAll('input.expenses-item'),
+    approve = document.getElementsByTagName('button')[0],
+    approve1 = document.getElementsByTagName('button')[1],
+    calculate = document.getElementsByTagName('button')[2],
+    inputOpt = document.querySelectorAll('.optionalexpenses-item'),
+    income = document.querySelectorAll('#income'),
+    savings = document.querySelector("#savings"),
+    sum = document.querySelector("#sum"),
+    percent= document.querySelector("#percent"),
+    year_value= document.querySelector(".year-value"),
+    month_value= document.querySelector(".month-value"),
+    day_value= document.querySelector(".day-value");
+
+console.log(day_value);
